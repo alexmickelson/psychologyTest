890436ac865bcb472b6f203e75b5029f07a5cb3c
alexmickelson <alexmickelson96@gmail.com>
2019-03-07 14:17:24 -0700

    started frontend

---
+++ frontend/index.js
@@ -0,0 +1,13 @@
+$( function(){
+    var start = $.now()
+    
+    $(document).on('keypress',function(e) {
+        if(e.which == 97) { //a
+            alert($.now()-start);
+        }
+        if(e.which == 100) { //d
+            alert($.now()-start);
+        }
+    });
+    //debugger;
+});
